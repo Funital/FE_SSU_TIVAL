107b9cab7d86dd52db2a5bcfc673abb4b555289a
Funital <gksrmawns0925@naver.com>
2024-11-24 14:53:19 +0900

    [noobi/week12_report]각각 합치기

---
+++ week12_report/try04/Fessutival_RE/app/src/main/java/com/example/fessutival_re/reservation/CompleteConfirmationActivity.java
@@ -0,0 +1,21 @@
+package com.example.fessutival_re.reservation;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.fessutival_re.R;
+
+public class CompleteConfirmationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_confirmation);
+
+        // 전달된 메시지를 받아와서 텍스트에 설정
+        String message = getIntent().getStringExtra("message");
+        TextView textView = findViewById(R.id.CompleteTextView2);
+        textView.setText(message);
+    }
+}
